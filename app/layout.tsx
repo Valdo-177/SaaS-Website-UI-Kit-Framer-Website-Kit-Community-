eb930f17d085413eb4348a1053f261699ccb3675
Valdo-117 <keyerdelahozsteam@gmail.com>
2024-08-24 00:06:57 -0500

    24 de agosto 12:06 PM

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { DM_Sans } from "next/font/google";
 import "./globals.css";
-import prev from '../public/PreviewSaas.png'
 
 const DMSans = DM_Sans({ subsets: ["latin"] });
 
@@ -18,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="es">
       <head>
-      <meta property="og:image" content={prev.src}></meta>
+      <meta property="og:image" content="https://firebasestorage.googleapis.com/v0/b/fb-picporter.appspot.com/o/HADA%2FPreviewSaas.png?alt=media&token=88c84399-0d48-4d8f-b0a6-1658d98c3149"></meta>
       <meta property="og:site_name" content="Link preview site name"></meta>
       <meta property="og:title" content="SaaS Website UI Kit — Framer Website Kit (Community)"></meta>
       <meta property="og:description" content="This project is developed with the purpose of evaluating and improving my skills in the design and development of static web pages. Through this project, I seek to challenge my ability to create attractive user interfaces, optimize the user experience" />
